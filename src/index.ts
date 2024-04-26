ea857e11e84100da0fb318d455cebec20d09b36b
Kapil-Kharera <kapil95881@gmail.com>
2024-04-27 01:04:04 +0530

    Basic express server setup with typescript

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import serverConfig from "./config/serverConfig";
+
+const { PORT } = serverConfig;
+
+const app = express();
+
+app.listen(PORT, () => {
+    console.log(`Server started at *: ${PORT}`);
+})
